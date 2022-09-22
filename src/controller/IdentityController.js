3a988baf651e88ed49ff9dffd66ab7897552aee3
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2022-09-21 22:59:12 -0300

    agregamos el id como respuesta en el login

--- src/controller/IdentityController.js
+++ src/controller/IdentityController.js
@@ -1,11 +1,16 @@
-const { post } = require('../utils/axios');
+const { post, get } = require('../utils/axios');
 const handlerError = require('../utils/handlerError');
 
 class UserController {
   async signIn(req, res, next) {
-    return post("http://login_microservice:5000/login", req.body)
-      .then(response => {
-        res.customResponse = { statusCode: 200, ...response.data };
+    return Promise.all([
+      get(`http://user_microservice:5000/users?email=${req.body.email}`),
+      post("http://login_microservice:5000/login", req.body)
+    ])
+      .then(([usersResponse, loginResponse]) => {
+        const id = usersResponse.data.data[0].id;
+        const token = loginResponse.data.token;
+        res.customResponse = { statusCode: 200, ...{ id, token } };
         next();
       })
       .catch(err => {
