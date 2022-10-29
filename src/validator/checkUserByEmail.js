5f49e062c195179519810e88e0e5128f0a7b5469
argelignacio <iargel@fi.uba.ar>
2022-10-29 13:22:05 -0300

    last configs

--- src/validator/checkUserByEmail.js
+++ src/validator/checkUserByEmail.js
@@ -1,7 +1,9 @@
+const { endpoints: { userMicroservice } } = require('config');
+
 const { post } = require("../utils/axios");
 
 module.exports = (req, res, next) => {
-  return post("http://user_microservice:5000/users/verifyUserByEmail", req.body)
+  return post(`${userMicroservice}/users/verifyUserByEmail`, req.body)
     .then(() => {
       next()
     })
--- src/validator/checkUserByEmailAndPassword.js
+++ src/validator/checkUserByEmailAndPassword.js
@@ -1,8 +1,11 @@
+const { endpoints: { userMicroservice } } = require('config');
+
 const handlerResponse = require("../utils/handlerResponse");
 const { get } = require("../utils/axios");
+const logger = require('../../winston');
 
 module.exports = (req, res, next) => {
-  return get(`http://user_microservice:5000/users/login?email=${req.body.email}&password=${req.body.password}`)
+  return get(`${userMicroservice}/users/login?email=${req.body.email}&password=${req.body.password}`)
     .then(({ data: { data } }) => {
       if (!data.length) {
         res.status(404).send({ message: 'El usuario no fue identificado' });
@@ -12,7 +15,7 @@ module.exports = (req, res, next) => {
       next();
     })
     .catch(err => {
-      console.log(err);
+      logger.error(err);
       const { statusCode, ...otherFields } = handlerResponse(err);
       res.status(statusCode).send(otherFields);
     });
