882c3b47df597d97ee47a84c39f900163157c868
facundoluzzi <fnluzzi@gmail.com>
2022-10-30 18:46:16 -0300

    fix en validar token

--- src/validator/validateToken.js
+++ src/validator/validateToken.js
@@ -4,13 +4,14 @@ const { post } = require("../utils/axios");
 const handlerResponse = require('../utils/handlerResponse');
 
 module.exports = (req, res, next) => {
-  const url = process.env.identity_microservice || endpoints.identityService;
+  const url = process.env.identity_microservice || endpoints.identityMicroservice;
 
   return post(`${url}/token`, {}, { authorization: req.headers.authorization })
     .then(() => {
       next()
     })
     .catch(err => {
+      console.log(err);
       const { statusCode, ...other } = handlerResponse(err);
       logger.error(JSON.stringify({ ...other, statusCode }));
       res.status(statusCode).send({ message: other });
